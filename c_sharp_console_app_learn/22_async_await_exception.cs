f9dd9e85aff0db8805f960a75ca4719e22df4fbd
Arun311092 <46894875+Arun311092@users.noreply.github.com>
2021-12-04 14:58:31 +0530

    Async await additional topics

---
+++ c_sharp_console_app_learn/22_async_await_exception.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_console_app_learn
+{
+    internal class _22_async_await_exception
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Method1();
+                Method2();
+                Console.ReadKey();
+            }
+
+            public static async Task Method1()
+            {
+                await Task.Run(() =>
+                {
+                    for (int i = 0; i < 25; i++)
+                    {
+                        Console.WriteLine(" Method 1 " + i);
+                        // Do something
+                        Task.Delay(100).Wait();
+                    }
+                    try
+                    {
+                        throw new Exception("my expn");
+                    } catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+
+                });
+            }
+
+
+            public static async Task Method2()
+            {
+                await Task.Run(() =>
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        Console.WriteLine(" Method 2 " + i);
+                        // Do something
+                        Task.Delay(100).Wait();
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ c_sharp_console_app_learn/22_async_await_params.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_console_app_learn
+{
+    internal class _22_async_await_params
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Method1("Hello");
+                Method2("World");
+                Console.ReadKey();
+            }
+
+            public static async Task Method1(String param)
+            {
+                await Task.Run(() =>
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        Console.WriteLine(param);
+                        // Do something
+                        Task.Delay(100).Wait();
+                    }
+                });
+            }
+
+            public static void Method2(String param)
+            {
+                for (int i = 0; i < 25; i++)
+                {
+                    Console.WriteLine(param);
+                    // Do something
+                    Task.Delay(100).Wait();
+                }
+            }
+        }
+    }
+}
