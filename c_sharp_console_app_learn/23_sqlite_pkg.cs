b82e4b43c166d0452fa40f40489fbe4f04a28616
Arun311092 <46894875+Arun311092@users.noreply.github.com>
2021-12-05 14:03:21 +0530

    SQLite db pkg

---
+++ c_sharp_console_app_learn/23_sqlite_pkg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace c_sharp_console_app_learn
+{
+    internal class _23_sqlite_pkg
+    {
+        static void Main(string[] args)
+        {
+            String createtable = @"create table if not exists students ( id INTEGER primary key, name VARCHAR(255), marks DOUBLE )";
+
+            System.Data.SQLite.SQLiteConnection.CreateFile("student.db");
+
+            using (System.Data.SQLite.SQLiteConnection connection = new System.Data.SQLite.SQLiteConnection("data source=student.db"))
+            {
+                using (System.Data.SQLite.SQLiteCommand command = new System.Data.SQLite.SQLiteCommand(connection))
+                {
+                    connection.Open();
+                    command.CommandText = createtable;
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "insert into students ( name, marks ) values ( 'a1', '87' )";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "insert into students ( name, marks ) values ( 'b2', '88' )";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "select * from students";
+                    using (System.Data.SQLite.SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader["name"] + " " + reader["marks"]);
+                        }
+                    }
+
+                    connection.Close();
+
+                }
+            }
+        }
+    }
+}
